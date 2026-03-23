84fc432898179187ab054d77715ca11f4274f8bc
sohamnesarikar <nesarikarsoham24@gmail.com>
2026-03-23 19:36:01 +0530

    handle invalid mongoose id error & validations error

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,10 +1,23 @@
 import { config } from "../config/config.js";
+import ApiError from "../utils/ApiError.js";
 
 export const errorMiddleware = (err, req, res, next) => {
-  const error = {
+  let error = {
     statusCode: err.statusCode || 500,
     message: err.message || "Internal server error",
   };
+
+  // Handle Invalid mongoose id error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new ApiError(404, message);
+  }
+
+  // Handle validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ApiError(400, message);
+  }
 
   if (config.NODE_ENV === "DEVELOPEMENT") {
     return res.status(error.statusCode).json({
